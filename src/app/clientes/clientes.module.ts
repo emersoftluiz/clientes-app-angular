ddcc940ae243b0985b1a8a8527d251ad0caf9cd6
Emersoft <emersonbsi@gmail.com>
2020-07-10 21:45:40 -0300

    Criando o módulo de clientes

---
+++ src/app/clientes/clientes.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ClientesRoutingModule } from './clientes-routing.module';
+import { ClienteFormComponent } from './cliente-form/cliente-form.component';
+
+
+@NgModule({
+  declarations: [ClienteFormComponent],
+  imports: [
+    CommonModule,
+    ClientesRoutingModule
+  ]
+})
+export class ClientesModule { }
